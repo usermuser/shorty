c7b53aa02c07816ae3985c276ef6a7fa1a5e43ed
usermuser <job.khusainov@gmail.com>
2023-12-19 19:26:14 +0500

    add user model

--- app/db.py
+++ app/db.py
@@ -15,11 +15,13 @@ class BaseMeta(ormar.ModelMeta):
 
 class User(ormar.Model):
     class Meta(BaseMeta):
+        database = database
+        metadata = metadata
         tablename = "users"
 
-    id: int = ormar.Integer(primary_key=True)
+    user_id: int = ormar.Integer(primary_key=True)
     email: str = ormar.String(max_length=128, unique=True, nullable=False)
-    active: bool = ormar.Boolean(default=True, nullable=False)
+    created: ormar.DateTime(timezone=True)
 
 
 engine = sqlalchemy.create_engine(settings.db_url)
